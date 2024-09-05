80b65f9a57f514078e98c9ffe760d18bdf29f5d9
arthur-plautz <atr.plautz@gmail.com>
2024-09-05 13:57:51 -0300

    Working on new models for critic

---
+++ exploration/critic/classification/utils.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+
+def graph_defaults():
+    import warnings
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    warnings.filterwarnings("ignore")
+    sns.set_theme()
+    sns.set(rc={'figure.figsize':(16,10)}, style='white')
+    plt.figure(dpi=600)
+
+def fit_cv(reg, data, model):
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    cwd = os.getcwd()
+    model_path = f"{cwd}/data/{model}.npy"
+    params = None
+
+    if os.path.exists(model_path):
+        _params = np.load(model_path, allow_pickle=True)
+        params = dict(_params.item())
+    else:
+        X, y = data
+        reg.fit(X, y)
+        params = reg.best_params_
+        np.save(model_path, params)
+
+    return params
+
+def get_label(y):
+    return (y-1) // 200
---
+++ exploration/critic/regression/critic.py
@@ -0,0 +1,49 @@
+import numpy as np
+from time import process_time
+from sklearn.preprocessing import PolynomialFeatures
+
+pf = PolynomialFeatures(2)
+
+def critic(c, m, X, y, model):
+    s, yp = None, None
+    if c:
+        yp = m.predict(X)
+        s = m.score(X, y)
+        c = 0
+    else:
+        m = model(X, y)
+        c = 1
+    return c, m, s, yp
+
+
+def evolution(X, y, model, start=10000, batch=10):
+    score = []
+    predictions = []
+
+    gmax = len(X)
+    m = model()
+    g = start
+    c = 0
+
+
+    _start = process_time()
+
+    while g < gmax:
+        gnext = g+batch
+        yg = np.array(y[g:gnext]).reshape(-1, 1)
+        Xg = pf.fit_transform(X[g:gnext], yg)
+
+        c, m, s, yp = critic(c, m, Xg, yg, model)
+            
+        g = gnext
+
+        if not c:
+            score.append(s)
+            predictions.append(yp)
+
+    _end = process_time()
+    _cost = _end - _start
+
+
+    return score, predictions, _cost
+
